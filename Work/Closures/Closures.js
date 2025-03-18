b2f46e58fa1d743e125c8c03cb3a1e640cb91538
Abhinavcv2002 <abhinavcv227@gmail.com>
2025-03-18 15:14:19 +0530

    Build in Methods

---
+++ Work/Closures/Closures.js
@@ -0,0 +1,13 @@
+function outerfun(outervar){
+    return function innerfun(bcd){
+        console.log(outervar);
+        console.log(bcd);
+        
+        
+    }
+}
+
+let a = outerfun(10);
+console.log(a);
+a(100)
+     
---
+++ java scripts/scripts3.js
@@ -0,0 +1,33 @@
+a = [1,2,3,4,5,6,7,8]
+
+    // a.foreach((item)=>console.log(item))
+    // const data = a.map((item)=>item*10)
+    // console.log(data);
+
+//-- filter Method --//
+
+        // const data = a.filter((item)=>item%2==0)
+        // console.log(data);
+    
+//-- filter Method (list type) --//
+    
+        // const data = ['abc','hello','asd']
+        // const result = data.filter((item)=>item[0]=='a')
+        // console.log(result);
+    
+
+// callback method --//
+
+//-- array.forEach(callback(Element,index,array),thisValue) --//
+
+        // function data(a,b,callback){
+        //     const c = a+b
+        //     callback(c)
+        // }
+        // function display(c){
+        //     console.log(c);
+            
+        // }
+
+        // data(10,2,display)
+        // data(112,2,display)
