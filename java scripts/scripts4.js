4b8d7cccb781f0060a952b2e8e70c4b7ecd97321
Abhinavcv2002 <abhinavcv227@gmail.com>
2025-03-20 15:59:01 +0530

    promise

--- java scripts/scripts4.js
+++ java scripts/scripts4.js
@@ -26,7 +26,7 @@
     //-- Two type of inheritance --//
 
 //-- 1. classical inheritance //-- 2. prototype inheritance --//
---//
+
 //--  classical inheritance was 5-types --//
 
         // class Synnefo{
@@ -77,3 +77,120 @@
         //    a1.great()
         //    console.log(a1);
        
+//-- Next --//
+
+//-- exception handling --//
+
+        // try {
+        //         console.log(a);
+        // }
+        // catch (error){
+        //         console.log('error'.error.message);
+        // }
+        // finally{
+        //         console.log('prgm ends');
+        // }
+
+
+        // try {
+        //      const a = parseInt('abc')
+        //      // console.log(a);
+        //      if (isNaN(a)) {
+        //         throw new error('a is not a number');
+        //      }
+        // }
+        // catch (error){
+        //      console.log('error'.error.message);
+        // }
+        // finally{        
+        //      console.log('prgm ends');
+        // }
+
+//-- call back and call back hell --//
+//-- maltiple task called call back hell --//
+
+        // function Todo(task,callback){
+        //         console.log('starting',task);
+        //         setTimeout(()=>{
+        //                 console.log('ending',task);
+        //                 callback();
+                        
+        //         },1000)
+                
+        // }
+
+        // function step1(callback){
+        //         setTimeout(()=>{
+        //                 console.log('step1 completed');
+        //                 callback();
+                        
+        //         },1000)
+        // }       
+
+        // function step2(callback){
+        //         setTimeout(()=>{
+        //                 console.log('step2 completed');
+        //                 callback();
+                        
+        //         },1000)
+        // }
+
+        // function step3(callback){
+        //         setTimeout(()=>{
+        //                 console.log('step3 completed');
+        //                 callback();
+                        
+        //         },1000)
+        // }
+
+        // step1(()=>{
+        //         step2(()=>{
+        //                 step3(()=>{
+        //                         console.log('all steps completed');
+                                
+        //                 })
+        //         })
+        // })
+
+
+//-- promise 3-type --//
+        // pending 
+        // fullfiled
+        // rejected
+        // resolve
+
+//-- callback isue resoled code using reject & resolve --//
+
+        // function step1(callback){
+        //         return new Promise ((resolve)=>{
+        //                 setTimeout(()=>{
+        //                         console.log('step1 completed');
+        //                         resolve(); 
+        //                 },1000)
+        //         })
+        // }       
+
+        // function step2(callback){
+        //         return new Promise ((resolve)=>{
+        //                 setTimeout(()=>{
+        //                         console.log('step2 completed');
+        //                         resolve(); 
+        //                 },1000)
+        //         })
+        // }    
+
+        // function step3(callback){
+        //         return new Promise ((resolve)=>{
+        //                 setTimeout(()=>{
+        //                         console.log('step3 completed');
+        //                         resolve(); 
+        //                 },1000)
+        //         })
+        // }    
+
+        // step1()
+        //      .then(step2)
+        //      .then(step2)
+        //      .then(()=>{
+        //         console.log('All steps completed');
+        //      })
